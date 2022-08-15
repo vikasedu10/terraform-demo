3f7e907fdffbe5b09b78bdc2a7a33365ce95de79
Vikas Bahuguna <vikasedu10@gmail.com>
2022-08-16 00:40:43 +0530

    Create subnet in default vpc
    Used 'data' component for getting default vpc and creating subnet in that vpc with cidr_range that does not already exists in that vpc

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ resource "aws_subnet" "dev-subnet-1" {
     cidr_block = "10.0.10.0/24"
     availability_zone = "us-east-1a"
 }
+
+data "aws_vpc" "default-vpc" {
+  default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+    vpc_id = data.aws_vpc.default-vpc.id
+    cidr_block = "172.31.128.0/20" # Must not be assigned to existing subnets in default vpc
+    availability_zone = "us-east-1a"
+}
