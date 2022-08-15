66f26ca60c74951e16962c04ccfaae86689fdfdc
Vikas Bahuguna <vikasedu10@gmail.com>
2022-08-16 02:45:52 +0530

    Update correct range (as per sequence 16, 32, 48, 64, 80, ..)

--- main.tf
+++ main.tf
@@ -4,12 +4,20 @@ provider "aws" {
 
 resource "aws_vpc" "dev-vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "Name" = "dev-vpc"
+  }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
     vpc_id = aws_vpc.dev-vpc.id
     cidr_block = "10.0.10.0/24"
     availability_zone = "us-east-1a"
+
+    tags = {
+      "Name" = "dev-subnet-1"
+    }
 }
 
 data "aws_vpc" "default-vpc" {
@@ -18,6 +26,10 @@ data "aws_vpc" "default-vpc" {
 
 resource "aws_subnet" "dev-subnet-2" {
     vpc_id = data.aws_vpc.default-vpc.id
-    cidr_block = "172.31.128.0/20" # Must not be assigned to existing subnets in default vpc
+    cidr_block = "172.31.96.0/20" # Must not be assigned to existing subnets in default vpc
     availability_zone = "us-east-1a"
+
+    tags = {
+      "Name" = "dev-subnet-2"
+    }
 }
